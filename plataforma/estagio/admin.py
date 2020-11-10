ae4b9c84512afce842caf4df1a48d914e9a73f5f
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2020-11-10 20:04:44 -0300

    Adicionado estagio/admin.py

--- plataforma/estagio/admin.py
+++ plataforma/estagio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ConvenioModel
+from .models import ConvenioModel, DocumentoEstagioModel
 
 
 class ConvenioModelAdmin(admin.ModelAdmin):
@@ -12,3 +12,16 @@ class ConvenioModelAdmin(admin.ModelAdmin):
 
 
 admin.site.register(ConvenioModel, ConvenioModelAdmin)
+
+
+class DocumentoEstagioModelAdmin(admin.ModelAdmin):
+    list_display = ('empresa', 'nome_aluno', 'tipo_documento', 'curso_fatec',
+                    'changed_at', 'created_at', 'documento',
+                    'observacao_professor','aprovado_professor',)
+    date_hierarchy = 'created_at'
+    search_fields = ('empresa', 'nome_aluno',
+                     'aprovado_professor', 'changed_at', 'created_at')
+    list_filter = ('empresa', 'nome_aluno', )
+
+
+admin.site.register(DocumentoEstagioModel, DocumentoEstagioModelAdmin)
