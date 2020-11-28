4047f306e04caf68bbbaf4e044b7c520e9123066
maeliseu <maeliseu@gmail.com>
2020-11-27 23:10:35 -0300

    Create API in app vitrine

--- plataforma/vitrine/urls.py
+++ plataforma/vitrine/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 app_name = 'vitrine'
@@ -7,4 +8,6 @@ urlpatterns = [
     path('', views.showcase, name='showcase'),
     path('cadastro', views.cadastro, name='cadastro'),
     path('remover', views.remover, name='remover'),
+    path('api/vitrine/', views.VitrineList.as_view(), name='vitrine-list'),
+    path('api/vitrine/<int:pk>/', views.VitrineDetail.as_view(), name='vitrine-Detail'),
 ]
